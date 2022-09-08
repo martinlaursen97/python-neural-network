9cb37ec3ee8517ef3aa6683cf37f28b2789a7331
martinlaursen97 <martin_laursen21@hotmail.com>
2022-09-08 23:22:18 +0200

    forward

--- layer.py
+++ layer.py
@@ -1,12 +1,27 @@
 import numpy as np
 
-np.random.seed(0)
+#np.random.seed(0)
 
 
 class Layer:
-    def __init__(self, inputs, neurons):
+    def __init__(self, input_amount, layer_size):
         # Every neuron will have 1 weight for every neuron in the previous layer
-        self.weights = np.random.randn(inputs, neurons)
+        self.weights = np.random.randn(input_amount, layer_size)
 
         # 1 bias for every neuron
-        self.biases = np.zeros((1, neurons))
+        self.biases = np.zeros((1, layer_size))
+
+    def forward(self, inputs):
+        # Need to calculate the outputs (w*i +b) of every neuron when forward propagating
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+        print(self.output)
+
+l_in = Layer(2, 2)
+l_out = Layer(2, 1)
+
+input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+input_targ = np.array([[0], [1], [1], [1]])
+
+l_in.forward(input_data)
+l_out.forward(l_in.output)
