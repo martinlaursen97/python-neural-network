db6b0e3a7188b341fcf44cd5b0841cec52424615
martinlaursen97 <martin_laursen21@hotmail.com>
2022-09-08 23:14:46 +0200

    layer class

--- layer.py
+++ layer.py
@@ -1,2 +1,12 @@
 import numpy as np
 
+np.random.seed(0)
+
+
+class Layer:
+    def __init__(self, inputs, neurons):
+        # Every neuron will have 1 weight for every neuron in the previous layer
+        self.weights = np.random.randn(inputs, neurons)
+
+        # 1 bias for every neuron
+        self.biases = np.zeros((1, neurons))
