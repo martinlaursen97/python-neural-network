a3dc16f813147e42e6b3b2f0970fcfffabc80650
martinlaursen97 <martin_laursen21@hotmail.com>
2022-09-09 21:54:25 +0200

    gradient, iteration

--- network.py
+++ network.py
@@ -2,7 +2,8 @@ import numpy as np
 import activation as a
 import loss as l
 
-#np.random.seed(0)
+
+# np.random.seed(0)
 
 
 class Network:
@@ -15,24 +16,35 @@ class Network:
     def insert_training_targets(self, training_targets):
         self.training_targets = training_targets
 
-    def train(self):
-        for i, t in zip(self.training_inputs, self.training_targets):
+    def train(self, iterations):
+        for i in range(iterations):
+            for i, t in zip(self.training_inputs, self.training_targets):
 
-            # Insert training data into input layer
-            self.layers[0].forward(i)
+                # Insert training data into input layer
+                self.layers[0].forward(i)
 
-            # Forward propagate remaining layers
-            for n, layer in enumerate(self.layers[1:], start=1):
-                prev_layer = self.layers[n - 1]
-                layer.forward(prev_layer.output)
+                # Forward propagate remaining layers
+                for n, layer in enumerate(self.layers[1:], start=1):
+                    prev_layer = self.layers[n - 1]
+                    layer.forward(prev_layer.output)
 
-            # Output of the output layer ([-1] == last layer)
-            actual_output = self.layers[-1].output
+                # Output of the output layer ([-1] == last layer)
+                actual_output = self.layers[-1].output
 
-            # Calculate loss
-            loss = l.mean_squared(actual_output, t)
+                # Calculate loss
+                loss = l.mean_squared(actual_output, t)
 
-            print(i, t, actual_output, loss)
+                print(i, t, actual_output, loss)
+
+                # Calculate gradient of output layer
+                self.layers[-1].calc_gradients(loss)
+
+                # Backward propagate remaining layers
+                for n, layer in reversed(list(enumerate(self.layers[:-1]))):
+                    prev_layer = self.layers[n + 1]
+                    layer.calc_gradients(prev_layer.gradients)
+
+                    #print(layer.gradients)
 
 
 class Layer:
@@ -45,16 +57,24 @@ class Layer:
 
     def forward(self, inputs):
         # Need to calculate the outputs (w*i +b) of every neuron when forward propagating
-        self.output = a.sigmoid(np.dot(inputs, self.weights) + self.biases)
+        output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = output
+        self.output = a.sigmoid(output)
+
+    def calc_gradients(self, prev):
+        # Prev layer gradients = loss if its the output layer
+
+        self.gradients = a.sigmoid(self.inputs) * (1 - a.sigmoid(self.inputs)) * prev
 
 
 l_in = Layer(2, 2)
+l_h = Layer(2, 2)
 l_out = Layer(2, 1)
 
 input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 input_targ = np.array([[0], [1], [1], [1]])
 
-network = Network([l_in, l_out])
+network = Network([l_in, l_h, l_out])
 network.insert_training_inputs(input_data)
 network.insert_training_targets(input_targ)
-network.train()
+network.train(200)
