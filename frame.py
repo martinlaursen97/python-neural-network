300cd6b6354e8d4c1990f50505f49860d9353a14
martinlaursen97 <martin_laursen21@hotmail.com>
2022-10-08 20:36:22 +0200

    frame finished

--- frame.py
+++ frame.py
@@ -1,9 +1,43 @@
 class Frame:
-    def __init__(self, file):
-        self.file = file
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.x = []
+        self.y = []
 
-    def transform(self, input_idx, output_idx):
-        pass
+    def transform(self, input_idx, output_from_idx=0, output_to_idx=-1):
+        file = open(self.file_path, 'r')
+        c = 0
+
+        while True:
+            c += 1
+
+            line = file.readline()
+            line_split = line.split(',')
+
+            if not line:
+                break
+
+            self.x.append([int(i) for i in line_split[input_idx]])
+            self.y.append([int(i) for i in line_split[output_from_idx:output_to_idx]])
+
+    def get_max(self):
+        max = 0
+        for i in self.y:
+            for j in i:
+                if j > max:
+                    max = j
+
+        return max
+
+    def normalize(self):
+        max = self.get_max()
+        self.y = [[i / max for i in j] for j in self.y]
 
 
+f = Frame("mnist_test.csv")
 
+f.transform(0, 1, -1)
+f.normalize()
+x, y = f.x, f.y
+
+print(y)
